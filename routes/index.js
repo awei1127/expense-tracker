be6f57640a2b6f05fd5407721f4f873c2db85acf
Wei <awei1127@hotmail.com>
2023-05-09 16:09:41 +0800

    feat: add register feature

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const dateTrans = require('../config/dateTrans')
 const RecordModel = require('../models/recordModel')
+const UserModel = require('../models/userModel')
 
 // 查
 router.get('/', async (req, res) => {
@@ -54,6 +55,21 @@ router.delete('/records/:id', async (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  // 預計在此顯示 兩次密碼不同 或任意欄位為空 的提示訊息
+  // 找該email是否已存在 若否則新增 若是則顯示提示並渲染register
+  const user = await UserModel.findOne({ email })
+  if (!user) {
+    await UserModel.create({ name, email, password })
+    console.log('user create success')
+    res.redirect('/login')
+  } else {
+    console.log('user exist')// 預計在此行顯示email已註冊的提示訊息
+    res.render('register', { name, email, password, confirmPassword })
+  }
 })
 
 router.get('/login', (req, res) => {
