be99282497a4e70fee0ac3fd6715eefca315493c
Wei <awei1127@hotmail.com>
2023-05-09 15:49:16 +0800

    feat: modify update(edit) feature

--- routes/index.js
+++ routes/index.js
@@ -28,13 +28,21 @@ router.post('/new', async (req, res) => {
   res.redirect('/')
 })
 
-// 改
 router.get('/records/:id/edit', async (req, res) => {
   const _id = req.params.id
   const record = await RecordModel.findOne({ _id })
   const { name, date, amount } = record
   const formatDate = dateTrans.formatDate(date)
-  res.render('edit', { name, date: formatDate, amount })
+  res.render('edit', { name, date: formatDate, amount, _id })
+})
+
+// 改
+router.put('/records/:id/edit', async (req, res) => {
+  const _id = req.params.id
+  const record = await RecordModel.findOne({ _id })
+  Object.assign(record, req.body) // 把使用者輸入的一筆資料賦值給從資料庫找出的一筆資料
+  await record.save()
+  res.redirect('/')
 })
 
 // 刪
